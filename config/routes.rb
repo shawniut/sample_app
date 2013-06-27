4c4e4f9771cfcb8e5517dabbd2aa71800fa415c1
shawniut <shawniut@gmail.com>
2013-06-26 22:32:14 -0600

    Basic devise tutorial is done!

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 SampleApp::Application.routes.draw do
   devise_for :users
 
+  resources :dashboard
+
+  root to: "home#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
